787cb34ad4bf62affe50bf7f38a032b9030bab09
DabeerM <dabeer.mirza@skyports.net>
2023-01-20 00:18:52 +0000

    Place the variable in a message

--- file_creator.py
+++ file_creator.py
@@ -3,9 +3,10 @@ import os
 # Specify the directory path
 path = os.getcwd()
 file_name = 'File.txt'
+msg = 'New file created!'
 
 # Creating a file at specified folder
 # join directory and file path
 with open(os.path.join(path, file_name), 'w') as fp:
     # uncomment below line if you want to create an empty file
-    fp.write('New file created!')
+    fp.write(msg)
